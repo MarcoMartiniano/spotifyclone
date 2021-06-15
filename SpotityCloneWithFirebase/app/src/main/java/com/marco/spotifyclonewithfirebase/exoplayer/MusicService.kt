99f968276fdc801abe2d7d40c22cde21fb78d977
MarcoMartiniano <79107727+MarcoMartiniano@users.noreply.github.com>
2021-06-15 15:41:48 -0300

    segundo commit
    segundo commit

--- SpotityCloneWithFirebase/app/src/main/java/com/marco/spotifyclonewithfirebase/exoplayer/MusicService.kt
+++ SpotityCloneWithFirebase/app/src/main/java/com/marco/spotifyclonewithfirebase/exoplayer/MusicService.kt
@@ -50,7 +50,7 @@ class MusicService : MediaBrowserServiceCompat() {
     private var isPlayerInitialized = false
     private lateinit var musicPlayerEventListener: MusicPlayerEventListener
 
-    companion object{
+    companion object {
         var curSongDuration = 0L
             private set
     }
@@ -61,8 +61,8 @@ class MusicService : MediaBrowserServiceCompat() {
             firebaseMusicSource.fetchMediaData()
         }
 
-        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let{
-            PendingIntent.getActivity(this,0,it,0)
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0, it, 0)
         }
 
         mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
@@ -76,11 +76,11 @@ class MusicService : MediaBrowserServiceCompat() {
             this,
             mediaSession.sessionToken,
             MusicPlayerNotificationListener(this)
-        ){
+        ) {
             curSongDuration = exoPlayer.duration
         }
 
-        val musicPlaybackPreparer = MusicPlaybackPreparer(firebaseMusicSource){
+        val musicPlaybackPreparer = MusicPlaybackPreparer(firebaseMusicSource) {
             curPlayingSong = it
             preparePlayer(
                 firebaseMusicSource.songs,
@@ -93,29 +93,27 @@ class MusicService : MediaBrowserServiceCompat() {
         mediaSessionConnector.setPlaybackPreparer(musicPlaybackPreparer)
         mediaSessionConnector.setQueueNavigator(MusicQueueNavigator())
         mediaSessionConnector.setPlayer(exoPlayer)
+
         musicPlayerEventListener = MusicPlayerEventListener(this)
         exoPlayer.addListener(musicPlayerEventListener)
         musicNotificationManager.showNotification(exoPlayer)
     }
 
-    private inner class MusicQueueNavigator : TimelineQueueNavigator(mediaSession){
+    private inner class MusicQueueNavigator : TimelineQueueNavigator(mediaSession) {
         override fun getMediaDescription(player: Player, windowIndex: Int): MediaDescriptionCompat {
             return firebaseMusicSource.songs[windowIndex].description
         }
     }
 
-
-
-    private fun preparePlayer (
+    private fun preparePlayer(
         songs: List<MediaMetadataCompat>,
         itemToPlay: MediaMetadataCompat?,
         playNow: Boolean
-    ){
+    ) {
         val curSongIndex = if(curPlayingSong == null) 0 else songs.indexOf(itemToPlay)
         exoPlayer.prepare(firebaseMusicSource.asMediaSource(dataSourceFactory))
         exoPlayer.seekTo(curSongIndex, 0L)
         exoPlayer.playWhenReady = playNow
-
     }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
@@ -126,6 +124,7 @@ class MusicService : MediaBrowserServiceCompat() {
     override fun onDestroy() {
         super.onDestroy()
         serviceScope.cancel()
+
         exoPlayer.removeListener(musicPlayerEventListener)
         exoPlayer.release()
     }
@@ -135,32 +134,44 @@ class MusicService : MediaBrowserServiceCompat() {
         clientUid: Int,
         rootHints: Bundle?
     ): BrowserRoot? {
-        return BrowserRoot(MEDIA_ROOT_ID,null)
+        return BrowserRoot(MEDIA_ROOT_ID, null)
     }
 
     override fun onLoadChildren(
         parentId: String,
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
-        when(parentId){
-            MEDIA_ROOT_ID ->{
+        when(parentId) {
+            MEDIA_ROOT_ID -> {
                 val resultsSent = firebaseMusicSource.whenReady { isInitialized ->
-                    if(isInitialized){
+                    if(isInitialized) {
                         result.sendResult(firebaseMusicSource.asMediaItems())
-                        if(!isPlayerInitialized && firebaseMusicSource.songs.isNotEmpty()){
-                            preparePlayer(firebaseMusicSource.songs,firebaseMusicSource.songs[0],false)
+                        if(!isPlayerInitialized && firebaseMusicSource.songs.isNotEmpty()) {
+                            preparePlayer(firebaseMusicSource.songs, firebaseMusicSource.songs[0], false)
                             isPlayerInitialized = true
                         }
-                    }else{
-                        mediaSession.sendSessionEvent(NETWORK_ERROR,null)
+                    } else {
+                        mediaSession.sendSessionEvent(NETWORK_ERROR, null)
                         result.sendResult(null)
                     }
                 }
-                if(!resultsSent){
+                if(!resultsSent) {
                     result.detach()
                 }
             }
-
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
